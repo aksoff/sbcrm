6e6336c93865636ac8fade18ff07f52420ee4c70
aksoff <an_popoff@inbox.ru>
2021-09-03 10:32:54 +0400

    admin media files set

--- crm/urls.py
+++ crm/urls.py
@@ -33,4 +33,4 @@ urlpatterns += [
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.BASE_DIR)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
