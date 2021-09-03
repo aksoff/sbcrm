7176749631dbe2b17704ec9c31c5cf5dc3c10f1d
Александр <alexn.popoff@gmail.com>
2021-09-03 22:50:26 +0400

    admin modify

--- crm/settings.py
+++ crm/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-puyief3iw*c*$9(z3j_9qh)fh4^k$brb)5xuuu_4b^td3l31dc'
+# SECRET_KEY = 'django-insecure-puyief3iw*c*$9(z3j_9qh)fh4^k$brb)5xuuu_4b^td3l31dc'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'puyief3iw*c*$9(z3j_9qh)fh4^k$brb)5xuuu_4b^td3l31dc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-#DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+# DEBUG = True
+# DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-#ADMIN_MEDIA_PREFIX = '/static/admin/'
+# ADMIN_MEDIA_PREFIX = '/static/admin/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
@@ -153,8 +153,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_SECURE = True
 
-#Settings for deploy to Heroku
-#wait..
+# Settings for deploy to Heroku
+# wait..
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
