c4ca16ec1dc962036f574d47ca70bba53f7d7871
Александр <alexn.popoff@gmail.com>
2021-09-05 15:11:52 +0400

    media

--- crm/settings.py
+++ crm/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 # ADMIN_MEDIA_PREFIX = '/static/admin/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
