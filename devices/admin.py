f5e55f0bef64161d425d313eca99bdbb1ae28b02
aksoff <an_popoff@inbox.ru>
2021-09-16 14:32:00 +0400

    autocomplete field brand

--- devices/admin.py
+++ devices/admin.py
@@ -3,7 +3,14 @@ from .models import DeviceType, Brand, DeviceModel
 
 # admin.site.register(DeviceModel)
 admin.site.register(DeviceType)
-admin.site.register(Brand)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    fields = ('name',)
+    list_editable = ('name',)
+    search_fields = ('name',)
 
 
 @admin.register(DeviceModel)
@@ -12,4 +19,5 @@ class DeviceModelAdmin(admin.ModelAdmin):
     fields = ('type', 'brand', 'name')
     list_editable = ('type', 'brand')
     search_fields = ('type__name', 'name', 'brand__name')
+    autocomplete_fields = ('brand',)
 
